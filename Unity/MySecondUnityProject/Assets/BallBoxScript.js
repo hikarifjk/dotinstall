086112b6f8ca39e4008d29442da2746f4605919c
Hikari Fujioka <hikarifjk65@gmail.com>
2015-11-13 06:09:39 +0900

    unity

---
+++ Unity/MySecondUnityProject/Assets/BallBoxScript.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var ball : Transform;
+
+function Update () {
+
+  if (Input.GetButtonUp("Jump")) {
+    Instrantiate(ball, transform.position, transform.rotation);
+    Debug.Log("Jumped!");
+  }
+}
---
+++ Unity/MySecondUnityProject/Assets/PlayerScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var x : int = 5;
+
+
+function Start () {
+  Debug.Log("Hello World -> " + x);
+}
+
+function Update () {
+
+  if (Input.GetButtonUp("Jump")) {
+    Debug.Log("Jumped!");
+  }
+  var xx : float = Input.GetAxis("Horizontal");
+  transform.Translate(xx * 0.2, 0, 0);
+  // transform.position.z += 0.1;
+  // transform.position += Vector3(0, 0, 0.1);
+  // transform.Translate(0, 0, 0.1);
+}
+function OnCollisionEnter(obj : Collision) {
+  if (obj.gameObject.name == 'RightWall') {
+    Debug.Log("Hit R");
+  }
+}

0b9f7ae3a2eb651aa4385833bbc26eb3c5435c36
vagrant <vagrant@localhost.localdomain>
2015-11-21 14:50:51 +0900

    rep PhpCalendar

--- PhpCalendar/index.php
+++ PhpCalendar/index.php
@@ -25,18 +25,27 @@
         <td>Fri</td>
         <td>Sat</td>
       </tr>
-      <?php $j = 0; ?>
+      <?php
+        $j = 0;
+        $calendarItemList = array(
+          1, 2, 3, 4, 5, 6, 7,
+          8, 9, 10, 11, 12, 13, 14,
+          15, 16, 17, 18, 19, 20, 21,
+          22, 23, 24, 25, 26, 27, 28,
+          29, 30, 1, 2, 3, 4, 5,
+        );
+       ?>
       <?php for ($i = 0; $i < 5; $i++) : ?>
         <tr>
         <?php for (; $j < 28; $j++) : ?>
             <?php if (($j + 1) % 7 == 1): ?>
-                <td class="sun"><?= $j + 1 ?></td>
+                <td class="sun"><?= $calendarItemList[$j] ?></td>
             <?php elseif (($j + 1) % 7 == 0): ?>
-                <td class="sat"><?= $j + 1 ?></td>
+                <td class="sat"><?= $calendarItemList[$j] ?></td>
                 <?php $j += 1; break; ?>
 
             <?php else: ?>
-                <td><?= $j + 1 ?></td>
+                <td><?= $calendarItemList[$j] ?></td>
             <?php endif; ?>
         <?php endfor; ?>
         </tr>

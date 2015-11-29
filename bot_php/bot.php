7abfe1853e16233363a1c8a653e9045295152241
hikarifjk <vagrant@localhost.localdomain>
2015-11-29 10:22:12 +0900

    bot

--- bot_php/bot.php
+++ bot_php/bot.php
@@ -10,7 +10,17 @@ $connection = new TwitterOAuth(
   ACCESS_TOKEN,
   ACCESS_TOKEN_SECRET);
 
-$content = $connection->get("account/vefity_credentials");
-$content = $connection->get("statuses/home_timeline", ['count'=>3]);
+// $content = $connection->get("account/vefity_credentials");
+// $content = $connection->get("statuses/home_timeline", ['count'=>3]);
 
-var_dump($content);
+$res = $connection->post("statuses/update", [
+  'status' => 'ドットインストールがおすすめ！ http://dotinstall.com #dotinstall'
+]);
+
+if ($connection->getLastHttpCode() === 200) {
+  echo "Success!" . PHP_EOL;
+} else {
+  echo "Error!" . $res->errors[0]->message . PHP_EOL;
+}
+
+//var_dump($content);

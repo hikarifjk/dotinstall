36ca4d1d89872adfeef902ec895cabd5f991703a
hikarifjk <vagrant@localhost.localdomain>
2015-12-06 06:51:43 +0900

    upload_php

--- upload_image_php/ImageUploader.php
+++ upload_image_php/ImageUploader.php
@@ -5,6 +5,9 @@ class ImageUploader {
   public function upload() {
     try {
       $this->_validateUpload();
+
+      $text = $this->validateImageType();
+
     } catch (\Exception $e){
       echo $e->getMessage();
       exit;
@@ -12,7 +15,39 @@ class ImageUploader {
     header('Location: http://' . $_SERVER['HTTP_HOST']);
     exit;
   }
-  private function _validateUpload() {
 
+  private function _validateImageType() {
+    $imageType = exif_iamge($_FILES['image']['tmp_name']);
+    switch ($imageType) {
+      case IMAGETYPE_GIF:
+        return 'gif';
+      case IMAGETYPE_JPEG:
+        return 'jpg';
+      case IMAGETYPE_PNG:
+        return 'png';
+      default:
+        throw new \Exception('PNG/JPEG/GIF only!');
+        break;
+    }
+  }
+
+
+  private function _validateUpload() {
+    var_dump($_FILES);
+
+    if (!isset($_FILES['image']['error'])) {
+      throw new \Exception('Upload Error');
+    }
+
+    switch ($_FILES['image']['error']) {
+      case UPLOAD_ERR_OK:
+        return true;
+        break;
+      case UPLOAD_ERR_INI_SIZE:
+      case UPLOAD_ERR_FORM_SIZE:
+        throw new \Exception('File too large!');
+      default:
+        throw new \Exception('Err: ' . $_FILES['image']['error']);
+    }
   }
 }

07827e22809361c3f2ff425590311d65efea15cc
hikarifjk <vagrant@localhost.localdomain>
2015-12-05 15:54:26 +0900

    upload

--- upload_image_php/ImageUploader.php
+++ upload_image_php/ImageUploader.php
@@ -3,6 +3,16 @@ namespace MyApp;
 
 class ImageUploader {
   public function upload() {
-    
+    try {
+      $this->_validateUpload();
+    } catch (\Exception $e){
+      echo $e->getMessage();
+      exit;
+    }
+    header('Location: http://' . $_SERVER['HTTP_HOST']);
+    exit;
+  }
+  private function _validateUpload() {
+
   }
 }
